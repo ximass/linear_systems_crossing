41ccd828773b1261c75c3565c2f46c55deb238d2
mateus schmitz <mateus.schmitz7@hotmail.com>
2023-11-18 23:53:55 -0300

    feat: inputs de entrada e configuração da exibição das imagens

---
+++ index.js
@@ -0,0 +1,28 @@
+$('input').blur(function () {
+    validateRow(this);
+});
+
+function validateRow(input) {
+    rowId = $(input).closest('[linha]').attr('linha');
+
+    var input01 = document.getElementById(rowId + '_1').value;
+    var input02 = document.getElementById(rowId + '_2').value;
+    var input03 = document.getElementById(rowId + '_3').value;
+    var input04 = document.getElementById(rowId + '_4').value;
+
+    if (input01 !== '' && input02 !== '' &&
+        input03 !== '' && input04 !== '') {
+        let proximaLinha = parseInt(rowId) + 1;
+
+        let proximaImagem = $(`#img_${proximaLinha}`);
+        proximaImagem.show();
+
+        let proximosInputs = $(`[linha="${proximaLinha}"]`).find('input');
+
+        if (proximosInputs.length != 0) {
+            $.each(proximosInputs, function (index, value) {
+                $(value).prop('disabled', false);
+            });
+        }
+    }
+}
