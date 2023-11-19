50dbef6d852f74036950871c295ebe641403f911
andre luiz braga mallmann <andrebmallmann@gmail.com>
2023-11-19 15:49:39 -0300

    validacao dos inputs inicias e dicas visuais apos apertar em conferir

--- index.js
+++ index.js
@@ -3,8 +3,114 @@ $('input').blur(function () {
 });
 
 $('#conferir').click(function () {
-    console.log('conferiu');
+    let validateFirstRow = changeInputColor(1, checkFirstRow());
+    let validateSecondRow = changeInputColor(2, checkSecondRow());
+    let validateThirdRow = changeInputColor(3, checkThirdRow());
+    let validateFourthRow = changeInputColor(4, checkFourthRow());
+
 });
+
+function changeInputColor(linha, boolean) {
+    var input01 = document.getElementById(linha + '_1').value;
+    var input02 = document.getElementById(linha + '_2').value;
+    var input03 = document.getElementById(linha + '_3').value;
+    var input04 = document.getElementById(linha + '_4').value;
+
+    if (boolean) {
+        document.getElementById(linha + '_1').style.backgroundColor = '#90EE90';
+        document.getElementById(linha + '_2').style.backgroundColor = '#90EE90';
+        document.getElementById(linha + '_3').style.backgroundColor = '#90EE90';
+        document.getElementById(linha + '_4').style.backgroundColor = '#90EE90';
+    } else {
+        document.getElementById(linha + '_1').style.backgroundColor = '#FF7F7F';
+        document.getElementById(linha + '_2').style.backgroundColor = '#FF7F7F';
+        document.getElementById(linha + '_3').style.backgroundColor = '#FF7F7F';
+        document.getElementById(linha + '_4').style.backgroundColor = '#FF7F7F';
+    }
+}
+
+
+function checkFirstRow() {
+    /*
+    entrada = 350 + 125 -> 475
+    saida = x + w ou w + x
+    */
+    let input01 = document.getElementById('1_1').value;
+    let input02 = document.getElementById('1_2').value;
+    let input03 = document.getElementById('1_3').value;
+    let input04 = document.getElementById('1_4').value;
+
+    //entrada
+    let carIn = parseInt(input01) + parseInt(input02);
+    let validateCarIn = carIn == 475;
+
+    let carOut = input03.toLowerCase() + input04.toLowerCase();
+    let validateCarOut = carOut == 'wx' || carOut == 'xw';
+
+    return validateCarIn && validateCarOut;
+}
+
+function checkSecondRow() {
+    /*
+        entrada x + y -> xy || yx
+        saida = 255 + 400 = 655
+    */
+    let input01 = document.getElementById('2_1').value;
+    let input02 = document.getElementById('2_2').value;
+    let input03 = document.getElementById('2_3').value;
+    let input04 = document.getElementById('2_4').value;
+
+    //entrada
+    let carIn = input01.toLowerCase() + input02.toLowerCase();
+    let validateCarIn = carIn == 'xy' || carIn == 'yx';
+
+    let carOut = parseInt(input03) + parseInt(input04);
+    let validateCarOut = carOut == 655;
+
+    return validateCarIn && validateCarOut;
+}
+
+function checkThirdRow() {
+    /*
+        entrada 800 + 250 = 1050
+        saida = y + z = yz || zy
+    */
+
+    let input01 = document.getElementById('3_1').value;
+    let input02 = document.getElementById('3_2').value;
+    let input03 = document.getElementById('3_3').value;
+    let input04 = document.getElementById('3_4').value;
+
+    //entrada
+    let carIn = parseInt(input01) + parseInt(input02);
+    let validateCarIn = carIn == 1050;
+
+    let carOut = input03.toLowerCase() + input04.toLowerCase();
+    let validateCarOut = carOut == 'yz' || carOut == 'zy';
+
+    return validateCarIn && validateCarOut;
+}
+
+function checkFourthRow() {
+    /*
+        entrada z + w = zw || wz
+        saida 300 + 600 = 900
+    */
+
+    let input01 = document.getElementById('4_1').value;
+    let input02 = document.getElementById('4_2').value;
+    let input03 = document.getElementById('4_3').value;
+    let input04 = document.getElementById('4_4').value;
+
+    let carIn = input01.toLowerCase() + input02.toLowerCase();
+    let validateCarIn = carIn == 'zw' || carIn == 'wz';
+
+    let carOut = parseInt(input03) + parseInt(input04);
+    let validateCarOut = carOut == 900;
+
+    return validateCarIn && validateCarOut;
+}
+
 
 function validateRow(input) {
     rowId = $(input).closest('[linha]').attr('linha');
@@ -29,8 +135,7 @@ function validateRow(input) {
             });
         }
 
-        if (proximaLinha == 5)
-        {
+        if (proximaLinha == 5) {
             $('#conferir').show();
         }
     }
